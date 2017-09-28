ee749ff42b2b9179bb5149a1b172f97ddadd5ba8
FlorentLequibain <florent.lequibain@gmail.com>
2017-09-28 10:15:17 +0200

    first commit

---
+++ 1modele/appli/req.php
@@ -0,0 +1,1 @@
+<?php include ("../connexion_sql.php") ?>
---
+++ 1modele/connexion_sql.php
@@ -0,0 +1,19 @@
+<?php
+
+try
+
+{
+
+    $bdd = new PDO('mysql:host=localhost;dbname=chantier;charset=utf8', 'root', 'Paperback1966');
+
+}
+
+catch(Exception $e)
+
+{
+
+        die('Erreur : '.$e->getMessage());
+
+}
+
+ ?>
